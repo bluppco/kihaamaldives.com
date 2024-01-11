e79fd0785dbd4c8ea7a3ee75673bbed65b095c00
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-01-11 14:50:44 +0530

    feat: created accommodation component

---
+++ src/atoms/headings/jsx/Four.jsx
@@ -0,0 +1,11 @@
+const HeadingFourJSX = ( props ) => {
+
+    return(
+        <h4 class="font-josefin font-light text-white text-lg text-center uppercase">
+            { props.children }
+        </h4>
+    )
+
+}
+
+export default HeadingFourJSX
--- src/components/home/AccommodationCarousel.jsx
+++ src/components/home/AccommodationCarousel.jsx
@@ -5,28 +5,35 @@ import { Carousel } from 'react-responsive-carousel'
 // IMPORTING CONSTANTS
 import { CDN_LINK, IMAGE_QUALITY } from "../../constants/cdn"
 
+// IMPORTING JSX ATOMS
+import HeadingFourJSX from "../../atoms/headings/jsx/Four"
+
 const HeroCarousel = ( props ) => {
 
     const data = Array(2).fill({
 
-        image: "hero-carousel-image-1.jpg"
+        image: "hero-carousel-image-1.jpg",
+        title: "Garden Villa with pool"
 
     })
 
     return (
-        <section className="w-full h-full">
-            <Carousel showThumbs={ false } showStatus={ false } showArrows={ true } autoPlay={ true } infiniteLoop={ true } stopOnHover={ false }>
+        <section className="w-full h-full relative">
+            <Carousel showThumbs={ false } showStatus={ false } showArrows={ true } autoPlay={ true } infiniteLoop={ true } stopOnHover={ false } showIndicators={ false }>
                 {
 
                     data.map( ( value, index ) => {
                         return (
                             <div key={ "hero-carousel-image-" + index } className="relative w-full">
-                                <div className="w-full h-screen bg-zinc-200">
+                                <div className="w-full h-96 bg-zinc-200">
                                     <img
                                         src={ value.image }
                                         alt=""
                                         className="w-full h-full object-cover"
                                     />
+                                </div>
+                                <div className="absolute bottom-0 flex justify-center w-full py-2 bg-black/20">
+                                <HeadingFourJSX>{ value.title }</HeadingFourJSX>
                                 </div>
                             </div>
                         )
