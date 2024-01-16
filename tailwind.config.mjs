53c9f0b6a12fef14c7d26d85f27438ae2f0560cb
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-01-16 12:37:09 +0530

    enhancement: updated screen size in tailwind config file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 
-		screens: {
-
-			'sm': '640px',
-			'md': '768px',
-			'lg': '1024px',
-			'xl': '1280px',
-			'mac': '1440px',
-			'2xl': '1536px'
-
-		},
-
 		extend: {
+
+			screens: {
+
+				'mac': '1440px',
+				...defaultTheme.screens,
+
+			},
 
 			fontFamily: {
 
