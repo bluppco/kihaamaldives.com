11e2fbe6b01d3d1663607bc1d3140fa37140396f
Mohit <mohit@hey.com>
2024-01-16 16:48:13 +0530

    x

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,45 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
 
-export default {
-
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: ["class"],
+  	content: [
+		'./pages/**/*.{js,jsx}',
+		'./components/**/*.{js,jsx}',
+		'./app/**/*.{js,jsx}',
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  	],
+	prefix: "",
 	theme: {
 
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
 		extend: {
+			keyframes: {
 
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+				'ping': 'ping 3s cubic-bezier(0,0,.2,1) infinite',
+			},
 			screens: {
 
 				'mac': '1440px'
-			},
 
+			},
 			fontFamily: {
 
 				"josefin": [ "Josefin Sans", "sans-serif" ],
@@ -25,13 +52,10 @@ export default {
 				"kmr_green": "#51c2b9",
 				"kmr_dim_gray": "#696969",
 
-			},
-			animation: {
-				'ping': 'ping 3s cubic-bezier(0,0,.2,1) infinite',
 			}
 
 		},
 	},
-	plugins: [],
+	plugins: [require("tailwindcss-animate")],
 
 }
